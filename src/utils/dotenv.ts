aa40c40516386be3c3342ebfb85a0c8e5e69895e
jksofm <lequochuypy1998@gmail.com>
2023-09-13 16:42:52 +0700

    update dotenv

--- src/utils/dotenv.ts
+++ src/utils/dotenv.ts
@@ -8,15 +8,15 @@ const env = process.env.NODE_ENV
 
 const envFilename = `.env.${env}`
 export const dotenvConfig = () => {
-  if (!env) {
-    console.log(`Ban chưa cung cấp biến môi trường NODE_ENV (ví dụ : development , production)`)
-    process.exit(1)
-  }
-  console.log(`Phat hiện NODE_ENV=${env}, vì thế app sẽ dùng file môi trường là ${envFilename}`)
-  if (!fs.existsSync(path.resolve(envFilename))) {
-    console.log(`Không tìm thấy file môi trường ${envFilename}`)
-    process.exit(1)
-  }
+  // if (!env) {
+  //   console.log(`Ban chưa cung cấp biến môi trường NODE_ENV (ví dụ : development , production)`)
+  //   process.exit(1)
+  // }
+  // console.log(`Phat hiện NODE_ENV=${env}, vì thế app sẽ dùng file môi trường là ${envFilename}`)
+  // if (!fs.existsSync(path.resolve(envFilename))) {
+  //   console.log(`Không tìm thấy file môi trường ${envFilename}`)
+  //   process.exit(1)
+  // }
   const pathFile = path.join(path.resolve(), env ? `/${envFilename}` : '/.env')
 
   dotenv.config({ path: pathFile })
