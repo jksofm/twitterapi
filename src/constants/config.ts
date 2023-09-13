9a2e4cf5720e081e1e594712e9b51b8bcb2eed8e
jksofm <lequochuypy1998@gmail.com>
2023-09-13 15:40:42 +0700

    update pm2

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,6 +1,7 @@
-import argv from 'minimist'
+// import argv from 'minimist'
 
 // Xu li moi truong dev
-const options = argv(process.argv.slice(2))
+// const options = argv(process.argv.slice(2))
+const env = process.env.NODE_ENV
 
-export const isProduction = Boolean(options.production)
+export const isProduction = env === 'production'
--- src/utils/dotenv.ts
+++ src/utils/dotenv.ts
@@ -1,9 +1,23 @@
 import path, { dirname } from 'path'
 import dotenv from 'dotenv'
+import fs from 'fs'
+
 import { isProduction } from '~/constants/config'
 
+const env = process.env.NODE_ENV
+
+const envFilename = `.env.${env}`
 export const dotenvConfig = () => {
-  const pathFile = path.join(path.resolve(), isProduction ? '/.env.production' : '/.env')
+  if (!env) {
+    console.log(`Ban chưa cung cấp biến môi trường NODE_ENV (ví dụ : development , production)`)
+    process.exit(1)
+  }
+  console.log(`Phat hiện NODE_ENV=${env}, vì thế app sẽ dùng file môi trường là ${envFilename}`)
+  if (!fs.existsSync(path.resolve(envFilename))) {
+    console.log(`Không tìm thấy file môi trường ${envFilename}`)
+    process.exit(1)
+  }
+  const pathFile = path.join(path.resolve(), env ? `/${envFilename}` : '/.env')
 
   dotenv.config({ path: pathFile })
 }
